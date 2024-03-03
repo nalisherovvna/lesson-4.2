a8d1f86e6fcca69af32ef4141ddc291ac685294e
nalisherovvna <mushknasiba@gmail.com>
2024-03-03 18:01:36 +0200

    it was my homework

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict '
+// bu cssda ishlatiladgan * orniga
+
+let String = prompt('String typdagi malumot')
+
+let Number = prompt('Number typdagi malmot kriting')
+
+let Boolen = prompt("Boolen typdagi malumot kriting")
+
+confirm("Hamma malumotlarni tog'ri kiritganingizga ishonchingiz komilmi?")
+
+alert('Siz kiritgan ' + String + ' sozi strign typega tegishli\n' + "Siz kiritgan " + Number + 'sozi number typega tegishli\n' + "Siz kiritgan" + Boolen + 'sozi boolen typega tegishli')
